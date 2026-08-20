986772581baa87111374043040ab58e34e33ec00
Cham Madamperuma <eng.cham.net@gmail.com>
2026-08-20 22:14:15 +0100

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // Custom domain deployment at https://chammadamperuma.com
-  base: '/'
+  // GitHub Pages repo site: https://cham.github.io/cham
+  base: '/cham/'
 })
